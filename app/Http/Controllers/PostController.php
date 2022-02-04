01bab9a145367e49abf0d9ffda7778eaa489e7fb
oumaima <lakhalouma6@gmail.com>
2022-02-05 00:21:54 +0100

    testss3

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+    public function postsTrashed()
+    {
+        $posts = Post::onlyTrashed()->where('user_id', Auth::id())->get();
+        return view('posts.trashed')->with('posts',$posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+    public function hdelete( $id)
+    {
+        $post = Post::withTrashed()->where('id' ,  $id )->first() ;
+        $post->forceDelete();
+        return redirect()->back() ;
+    }
+
+    public function restore( $id)
+    {
+        $post = Post::withTrashed()->where('id' ,  $id )->first() ;
+        $post->restore();
+        return redirect()->back() ;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,32 @@ Route::get('/', function () {
 
 Route::get('/home','HomeController@index')->name('home') ;
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Auth::routes() ;
 
 //route for profile
 Route::get('/profile', 'ProfileController@index' )->name('profile');
 Route::put('/profile/update', 'ProfileController@update' )->name('profile.update');
 
-Auth::routes() ;
+//route for posts
+Route::get('/posts', 'PostController@index' )->name('posts');
+Route::get('/post/create', 'PostController@create' )->name('post.create');
+Route::post('/post/store', 'PostController@store' )->name('post.store');
+Route::get('/post/show/{slug}', 'PostController@show' )->name('post.show');
+Route::get('/post/edit/{id}', 'PostController@edit' )->name('post.edit');
+Route::post('/post/update/{id}', 'PostController@update' )->name('post.update');
+Route::get('/post/destroy/{id}', 'PostController@destroy' )->name('post.destroy');
+Route::get('/post/hdelete/{id}', 'PostController@hdelete' )->name('post.hdelete');
+Route::get('/post/restore/{id}', 'PostController@restore' )->name('post.restore');
+Route::get('/posts/trashed', 'PostController@postsTrashed' )->name('posts.trashed');
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+////
 
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+
+
