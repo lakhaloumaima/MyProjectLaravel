ce66612791138298f3963a441cb16ab639200695
oumaima <lakhalouma6@gmail.com>
2022-02-05 01:39:27 +0100

    testss4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -16,12 +22,14 @@ class PostController extends Controller
     public function index()
     {
         //
+        $posts = Post::all() ;
+        return view('posts.index')->with('posts' , $posts) ;
 
     }
     public function postsTrashed()
     {
         $posts = Post::onlyTrashed()->where('user_id', Auth::id())->get();
-        return view('posts.trashed')->with('posts',$posts);
+        return view('posts.trash')->with('posts',$posts);
     }
 
     /**
@@ -31,7 +39,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create')  ;
     }
 
     /**
@@ -42,7 +50,25 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request , [
+            'title' => 'required' ,
+            'content' => 'required'  ,
+            'photo' => 'required|image'
+        ])  ;
+        $photo = $request->photo;
+        $newPhoto = time().$photo->getClientOriginalName();
+        $photo->move('uploads/posts'.$newPhoto) ;
+
+        $post = Post::create([
+            'user_id' =>  Auth::id(),
+            'title' =>  $request->title,
+            'content' =>   $request->content,
+            'photo' =>  'uploads/posts/'.$newPhoto,
+            'slug' =>  Str::slug($request->title),
+        ]);
+
+        return redirect()->back() ;
+
     }
 
     /**
@@ -53,7 +79,10 @@ class PostController extends Controller
      */
     public function show( $slug)
     {
-        //
+        $post = Post::where('slug' , $slug)->first() ;
+
+        return view('posts.show')->with('post' , $post) ;
+
     }
 
     /**
@@ -64,7 +93,9 @@ class PostController extends Controller
      */
     public function edit( $id)
     {
-        //
+        $post = Post::find($id) ;
+
+        return view('posts.edit')->with('post' , $post) ;
     }
 
     /**
@@ -76,7 +107,26 @@ class PostController extends Controller
      */
     public function update(Request $request,  $id)
     {
-        //
+        $post = Post::find($id) ;
+        $this->validate($request , [
+            'title' => 'required' ,
+            'content' => 'required'  ,
+            'photo' => 'required|image'
+        ])  ;
+
+        if($request->has('photo'))
+        {
+            $photo = $request->photo;
+            $newPhoto = time().$photo->getClientOriginalName();
+            $photo->move('uploads/posts'.$newPhoto) ;
+            $post->photo ='uploads/posts/'.$newPhoto ;
+        }
+
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->save();
+
+        return redirect()->back() ;
     }
 
     /**
@@ -87,7 +137,10 @@ class PostController extends Controller
      */
     public function destroy( $id)
     {
-        //
+        $post = Post::find($id) ;
+        $post->delete() ;
+        return redirect()->back() ;
+
     }
     public function hdelete( $id)
     {
